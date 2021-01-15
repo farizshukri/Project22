699aa23da51ff30b863b4b5dfa1fa29a33b1ab82
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2021-01-15 18:20:30 +0800

    initial commit

---
+++ music.go
@@ -0,0 +1,13 @@
+music-website/
+├── backend/
+│   ├── models/
+│   │   └── Music.js
+│   ├── routes/
+│   │   └── music.js
+│   ├── app.js
+│   └── package.json
+├── frontend/
+│   ├── index.html
+│   ├── style.css
+│   └── script.js
+└── README.md
