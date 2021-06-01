b56321b33fcda99ec5f97f3c20b14a2578c1551e
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2021-06-01 18:20:30 +0800

    initial commit

--- music-M.js
+++ music-M.js
@@ -1,2 +1,26 @@
-// backend/models/Music.js
-// Not needed in this simple example, but you can create a model layer if using an ORM like Sequelize
+// backend/routes/music.js
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./music.db');
+
+router.get('/', (req, res) => {
+    db.all('SELECT * FROM music', [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+router.post('/', (req, res) => {
+    const { title, artist, file } = req.body;
+    db.run('INSERT INTO music (title, artist, file) VALUES (?, ?, ?)', [title, artist, file], function (err) {
+        if (err) {
+            return res.status(400).json({ error: err.message });
+        }
+        res.status(201).json({ id: this.lastID });
+    });
+});
+
+module.exports = router;
