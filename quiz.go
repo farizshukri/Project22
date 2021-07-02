f6d5b911f69d39bb3eefa41fe9e4e2aa73d5a67d
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2021-07-02 18:20:30 +0800

    initial commit

---
+++ quiz.go
@@ -0,0 +1,11 @@
+quiz-website/
+├── backend/
+│   ├── app.js
+│   ├── routes/
+│   │   └── quiz.js
+│   └── package.json
+├── frontend/
+│   ├── index.html
+│   ├── style.css
+│   └── script.js
+└── README.md
