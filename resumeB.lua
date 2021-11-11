3d9010cb739e405fa48e885865b65ca64e7566d5
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2021-11-11 18:20:30 +0800

    initial commit

---
+++ resumeB.lua
@@ -0,0 +1,11 @@
+resume-builder/
+├── backend/
+│   ├── app.js
+│   ├── routes/
+│   │   └── resume.js
+│   └── package.json
+├── frontend/
+│   ├── index.html
+│   ├── style.css
+│   └── script.js
+└── README.md
