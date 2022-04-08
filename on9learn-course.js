049a122be573dded39eeb1e2cbde22485f1e2ece
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2022-04-08 18:20:30 +0800

    initial commit

---
+++ on9learn-course.js
@@ -0,0 +1,9 @@
+// backend/models/Course.js
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
