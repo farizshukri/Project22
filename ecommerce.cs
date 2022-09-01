f7de04ebc96ee211f2fadc8162dd561aede448f3
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2022-09-01 18:20:30 +0800

    initial commit

---
+++ ecommerce.cs
@@ -0,0 +1,20 @@
+ecommerce-site/
+│
+├── index.html
+├── products.html
+├── product-details.html
+├── cart.html
+├── checkout.html
+├── contact.html
+├── css/
+│   └── styles.css
+├── js/
+│   └── scripts.js
+├── images/
+│   └── (store your images here)
+├── php/
+│   ├── db.php
+│   ├── add-to-cart.php
+│   ├── checkout.php
+│   └── get-products.php
+└── .htaccess
